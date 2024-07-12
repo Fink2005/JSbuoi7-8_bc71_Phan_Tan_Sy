f859db89d6bd0cb73518e860f7e91d8e4c53a418
Fink2005 <phans3806@gmail.com>
2024-07-12 13:07:30 +0700

    deadline

---
+++ index.js
@@ -0,0 +1,133 @@
+// 1
+function click1() {
+    var change = document.querySelector(".show1")
+    if (change.style.display === "none")
+    {
+
+       change.style.display  = "block"
+    }
+    else { change.style.display  = "none"}
+}
+
+
+
+
+// 2
+
+
+
+function click2() {
+    var change = document.querySelector(".show2")
+    if (change.style.display === "none")
+    {
+
+       change.style.display  = "block"
+    }
+    else { change.style.display  = "none"}
+}
+
+
+
+// 3
+
+function click3() {
+    var change = document.querySelector(".show3")
+    if (change.style.display === "none")
+    {
+
+       change.style.display  = "block"
+    }
+    else { change.style.display  = "none"}
+}
+
+
+
+
+// 4
+function click4() {
+    var change = document.querySelector(".show4")
+    if (change.style.display === "none")
+    {
+
+       change.style.display  = "block"
+    }
+    else { change.style.display  = "none"}
+}
+
+
+// 5
+
+function click5() {
+    var change = document.querySelector(".show5")
+    if (change.style.display === "none")
+    {
+
+       change.style.display  = "block"
+    }
+    else { change.style.display  = "none"}
+}
+
+
+
+// 6
+
+function click6() {
+    var change = document.querySelector(".show6")
+    if (change.style.display === "none")
+    {
+
+       change.style.display  = "block"
+    }
+    else { change.style.display  = "none"}
+}
+
+
+
+
+// 7 
+function click7() {
+    var change = document.querySelector(".show7")
+    if (change.style.display === "none")
+    {
+
+       change.style.display  = "block"
+    }
+    else { change.style.display  = "none"}
+}
+
+
+
+
+// 8
+
+function click8() {
+    var change = document.querySelector(".show8")
+    if (change.style.display === "none")
+    {
+
+       change.style.display  = "block"
+    }
+    else { change.style.display  = "none"}
+}
+// 9
+
+function click9() {
+    var change = document.querySelector(".show9")
+    if (change.style.display === "none")
+    {
+
+       change.style.display  = "block"
+    }
+    else { change.style.display  = "none"}
+}
+// 10
+
+function click10() {
+    var change = document.querySelector(".show10")
+    if (change.style.display === "none")
+    {
+
+       change.style.display  = "block"
+    }
+    else { change.style.display  = "none"}
+}
